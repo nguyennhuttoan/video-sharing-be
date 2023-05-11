ab5e4eff2997bcdb2be8a0fd6a8835fb8a146283
Toan Nguyen Nhut <63847687+nguyennhuttoan@users.noreply.github.com>
2023-05-11 17:03:09 +0700

    Add unit tests for services

--- src/auth/service/auth.service.spec.ts
+++ src/auth/service/auth.service.spec.ts
@@ -1,18 +1,77 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from '../schema/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  const mockUser = {
+    email: 'test@gmail.com',
+    password: 'test1234',
+  };
+
+  const mockToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NWM4MGVlM2E0NmVlMTI4NzczNGYxNiIsImlhdCI6MTY4Mzc4NzQ5MiwiZXhwIjoxNjgzODczODkyfQ.n8FTPHNwjpkxZjxae3xvIzQfLDbpecV2sBC6461dx0Y';
+
+  let authService: AuthService;
+
+  const mockUserModel = {
+    findOne: jest.fn(() => {
+      return {
+        mockToken,
+      };
+    }),
+
+    create: jest.fn(() => {
+      return {
+        mockToken,
+      };
+    }),
+  };
+
+  const mockJwtService = {
+    sign: jest.fn(() => {
+      return {
+        token: mockToken,
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      providers: [
+        AuthService,
+        JwtService,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockUserModel,
+        },
+      ],
+    })
+      .overrideProvider(JwtService)
+      .useValue(mockJwtService)
+      .compile();
 
-    service = module.get<AuthService>(AuthService);
+    jest.spyOn(bcrypt, 'compare').mockImplementation(() => true);
+    jest.spyOn(bcrypt, 'hash').mockImplementation(() => mockUser.password);
+
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('should signin', async () => {
+    const res = await authService.signIn(mockUser);
+
+    expect(res.token).toEqual({ token: mockToken });
+  });
+
+  it('should signup', async () => {
+    const res = await authService.signUp(mockUser);
+
+    expect(res.token).toEqual({ token: mockToken });
   });
 });
--- src/video/service/video.service.spec.ts
+++ src/video/service/video.service.spec.ts
@@ -1,18 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VideoService } from './video.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Video } from '../schema/video.schema';
+import { User } from 'src/auth/schema/user.schema';
 
 describe('VideoService', () => {
-  let service: VideoService;
+  const req = {
+    user: {
+      email: 'test@gmail.com',
+      password: 'test1234',
+    },
+  };
+
+  const mockVideo = {
+    id: (Math.random() + 1).toString(36).substring(2),
+    title: 'test',
+    description: 'test',
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    user: req.user as User,
+  };
+
+  const mockVideos = [mockVideo];
+
+  let videoService: VideoService;
+  const mockVideoModel = {
+    find: jest.fn(() => {
+      return mockVideos;
+    }),
+    create: jest.fn(() => {
+      return {
+        ...mockVideo,
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VideoService],
+      providers: [
+        VideoService,
+        {
+          provide: getModelToken(Video.name),
+          useValue: mockVideoModel,
+        },
+      ],
     }).compile();
 
-    service = module.get<VideoService>(VideoService);
+    videoService = module.get<VideoService>(VideoService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(videoService).toBeDefined();
+  });
+
+  it('should get all videos', async () => {
+    const res = await videoService.findAll();
+
+    expect(res).toEqual(mockVideos);
+  });
+
+  it('should get all videos', async () => {
+    const res = await videoService.create(mockVideo, req.user as User);
+
+    expect(res).toEqual(mockVideo);
   });
 });
