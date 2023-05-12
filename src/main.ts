e40a658ae65397c82a90f60fdff53b4b315985fb
Toan Nguyen Nhut <63847687+nguyennhuttoan@users.noreply.github.com>
2023-05-12 16:19:31 +0700

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
 
